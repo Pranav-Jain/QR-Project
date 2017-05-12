44c01091dbf91e2f51cb8818b4e71d688154a905
Neelabhro <neelabhro16171@iiitd.ac.in>
2017-05-12 19:31:35 +0500

    using fnmatch

--- master_program.py
+++ master_program.py
@@ -10,34 +10,43 @@ import urllib
 import glob
 import os
 import shutil
-print("Hello")
+import fnmatch
+
 global slave_file
-slave_file = "slave.py"
+
+
 def check_update():
+        print(slave_file)
 	file = [{'input': slave_file}]
 	print("Update checking")
 	s = json.dumps(file)
 	#global slave_file = "lcdqr.py"
 	try:
-		res = requests.post("http://192.168.1.111:5000/check/", json=s).json()
+		res = requests.post("http://192.168.43.114:5000/check/", json=s).json()
 		check = res['check']
-		rec_file_name_dir=os.path.join(os.getcwd,res['file_name'])
+		rec_file_name = res['file_name']
+		rec_file_name_dir=os.path.join(os.getcwd(),res['file_name'])
 		print (check)
 		print (rec_file_name)
+		print (rec_file_name_dir)
+		
 		if(res['url']==''):
 			print ('File Not Found')
 		else:
 			print (res['url'])
 			try:
-				urllib.urlretrieve(res['url'],os.path.join(update_directory,rec_file_name))
+				urllib.urlretrieve(res['url'],os.path.join(update_directory,rec_file_name_dir))
 				print ("file retrieved")
-				command = 'ps aux | grep name | grep -v grep | awk "{print $2}"'
-				process = sp.call(command, shell = True)
-				sp.call('pkill -9 '+process, shell=True)
-				delete_cmd = "rm "+slave_file
-				sp.call(delete_cmd , shell=True)
+				#process = "ps aux | grep \"[p]ython3 " + slave_file +"\" | awk \'{print $2}\'"
+				#print(process)
+				#sp.call(process,shell=True)
+				#kill_proc = 'pkill -9 '+process[0]
+				#sp.call(kill_proc, shell=True)
+				#print("proc killed")
+				os.remove(slave_file)
+				print("file removed")
 				shutil.move(os.path.join(update_directory,rec_file_name),os.getcwd())
-				os.rename(rec_file_name,slave_file)
+				#os.rename(rec_file_name,slave_file)
 				os.execv(sys.executable, ['python'] + sys.argv)
 				print("update completed")
 			except:
@@ -51,6 +60,9 @@ update_directory = os.path.join(os.getcwd(),"update")
 if not os.path.exists(update_directory):
     os.makedirs(update_directory)
 
+for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, 'lcdqr*.py'):
+                slave_file = file
 check_update()
 #cmd = "python3 " + slave_file
 #sp.Popen(cmd, shell=True)
