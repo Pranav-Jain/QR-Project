99f4de00a813bdb771773467db9801e072c673d6
Neelabhro <neelabhro16171@iiitd.ac.in>
2017-05-12 17:26:12 +0500

    glob giving error.

--- master_program.py
+++ master_program.py
@@ -9,37 +9,42 @@ import requests
 import urllib
 import glob
 import os
-
+print("Hello")
 global slave_file
-slave_file = glob.glob('lcdqr*.py')
+slave_file = str(glob.glob('lcdqr*.py'))
 def check_update():
 	file = [{'input': slave_file}]
+	print("Update checking")
 	s = json.dumps(file)
 	#global slave_file = "lcdqr.py"
 	try:
-		res = requests.post("http://127.0.0.1:5000/check/", json=s).json()
+		res = requests.post("http://192.168.1.111:5000/check/", json=s).json()
 		check = res['check']
-		print check
+		rec_file_name=res['file_name']
+		print (check)
+		print (rec_file_name)
 		if(res['url']==''):
-			print 'File Not Found'
+			print ('File Not Found')
 		else:
-			print res['url']
+			print (res['url'])
 			try:
-				urllib.urlretrieve(res['url'])
+				urllib.urlretrieve(res['url'],rec_file_name)
 				print ("file retrieved")
 				command = 'ps aux | grep name | grep -v grep | awk "{print $2}"'
 				process = sp.call(command, shell = True)
 				sp.call('pkill -9 '+process, shell=True)
-				sp.call('rm '+ slave_file, shell=True)
+				delete_cmd = "rm "+slave_file
+				sp.call(delete_cmd , shell=True)
 				os.execv(sys.executable, ['python'] + sys.argv)
+				print("update completed")
 			except:
-				pass
+				print("inner except")
 		#os.remove(slave_file)
 	except:
-		pass
-	threading.Timer(5, check_update).start()	
+		print("outer except")
+	threading.Timer(2, check_update).start()	
 
 
 check_update()
-cmd = "python3 " + slave_file
-sp.Popen(cmd, shell=True)
+#cmd = "python3 " + slave_file
+#sp.Popen(cmd, shell=True)
