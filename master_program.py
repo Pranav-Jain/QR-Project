1227c20513cb228c0f28a404cbdc509e0143b39e
Neelabhro <neelabhro16171@iiitd.ac.in>
2017-05-13 12:27:08 +0500

    master program updated& working

--- master_program.py
+++ master_program.py
@@ -7,44 +7,55 @@ import sys
 import json
 import requests
 import urllib
-import glob
 import os
-print("Hello")
-global slave_file
-slave_file = str(glob.glob('lcdqr*.py'))
+import fnmatch
+
 def check_update():
+        slave_file = find_slave_file()                
+        print(slave_file)
 	file = [{'input': slave_file}]
-	print("Update checking")
+	print("checking for update...")
 	s = json.dumps(file)
-	#global slave_file = "lcdqr.py"
 	try:
-		res = requests.post("http://192.168.1.111:5000/check/", json=s).json()
+		res = requests.post("http://192.168.1.102:5000/check/", json=s).json()
 		check = res['check']
-		rec_file_name=res['file_name']
+		rec_file_name = res['file_name']
 		print (check)
 		print (rec_file_name)
+		
 		if(res['url']==''):
-			print ('File Not Found')
+			print ('Update Not Found')
 		else:
 			print (res['url'])
 			try:
 				urllib.urlretrieve(res['url'],rec_file_name)
 				print ("file retrieved")
-				command = 'ps aux | grep name | grep -v grep | awk "{print $2}"'
-				process = sp.call(command, shell = True)
-				sp.call('pkill -9 '+process, shell=True)
-				delete_cmd = "rm "+slave_file
-				sp.call(delete_cmd , shell=True)
-				os.execv(sys.executable, ['python'] + sys.argv)
+				sp.call("pkill python3",shell=True)
+				print("proc killed")
+				os.remove(slave_file)
+				print("file removed")				
 				print("update completed")
+				run_slave(rec_file_name)
 			except:
 				print("inner except")
-		#os.remove(slave_file)
 	except:
-		print("outer except")
-	threading.Timer(2, check_update).start()	
+		print("error contacting server")
+	threading.Timer(2, check_update).start()
+
+def run_slave(slave_file):
+        cmd = "python3 " + slave_file
+        sp.Popen(cmd, shell=True)
 
 
-check_update()
-#cmd = "python3 " + slave_file
-#sp.Popen(cmd, shell=True)
+def find_slave_file():
+        for file in os.listdir('.'):
+                if fnmatch.fnmatch(file, 'slave*.py'):
+                        return str(file)
+
+try:
+        run_slave(find_slave_file())
+        print("slave file started running")
+        
+except:
+        print("error running slave file")
+check_update()                        
